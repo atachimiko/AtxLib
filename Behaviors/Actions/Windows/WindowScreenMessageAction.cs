fe768beb244395b50d8ea4cffadce61103c574af
atachimiko <atachi.miko@gmail.com>
2013-06-21 14:50:24 +0900

    Initialize Repository

---
+++ Behaviors/Actions/Windows/WindowScreenMessageAction.cs
@@ -0,0 +1,31 @@
+﻿using AtxLib.Messaging.Windows;
+using Livet.Behaviors.Messaging;
+using Livet.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AtxLib.Behaviors.Actions.Windows
+{
+	/// <summary>
+	/// Windowのサイズや位置の設定を行うアクションです。
+	/// WindowScreenActionMessageに対応します。
+	/// </summary>
+	public class WindowScreenMessageAction : InteractionMessageAction<FrameworkElement>
+	{
+		protected override void InvokeAction(InteractionMessage message)
+		{
+			var windowScreenMessage = message as WindowScreenMessage;
+			if (windowScreenMessage != null)
+			{
+				var window = Window.GetWindow(AssociatedObject);
+
+				if (windowScreenMessage.WindowWidth > 0.0)
+					window.Width = windowScreenMessage.WindowWidth;
+			}
+		}
+	}
+}
---
+++ Messaging/Windows/WindowScreenMessage.cs
@@ -0,0 +1,45 @@
+﻿using Livet.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AtxLib.Messaging.Windows
+{
+	/// <summary>
+	/// Windowのサイズや位置を変更するための相互作用メッセージです。
+	/// </summary>
+	public class WindowScreenMessage : InteractionMessage
+	{
+		public WindowScreenMessage() { }
+
+		/// <summary>
+		/// メッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
+		/// </summary>
+		/// <param name="messageKey">メッセージキー</param>
+		public WindowScreenMessage(string messageKey)
+			: base(messageKey) { }
+
+		/// <summary>
+		/// 派生クラスでは必ずオーバーライドしてください。Freezableオブジェクトとして必要な実装です。<br/>
+		/// 通常このメソッドは、自身の新しいインスタンスを返すように実装します。
+		/// </summary>
+		/// <returns>自身の新しいインスタンス</returns>
+		protected override Freezable CreateInstanceCore()
+		{
+			return new WindowScreenMessage(MessageKey);
+		}
+
+		public Double WindowWidth
+		{
+			get { return (Double)GetValue(WindowWidthProperty); }
+			set { SetValue(WindowWidthProperty, value); }
+		}
+
+		public static readonly DependencyProperty WindowWidthProperty =
+			DependencyProperty.Register("WindowWidth", typeof(Double), typeof(WindowScreenMessage), new PropertyMetadata(0.0));
+
+	}
+}
