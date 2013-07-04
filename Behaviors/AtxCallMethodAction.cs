2c4a57c2333df6983d83fe69fa0403ca1803aae1
atachimiko <atachi.miko@gmail.com>
2013-07-04 20:00:22 +0900

    * [ADD] AtxCallMethodAction.cs

---
+++ Behaviors/AtxCallMethodAction.cs
@@ -0,0 +1,19 @@
+﻿using Livet.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtxLib.Behaviors
+{
+	public class AtxCallMethodAction : LivetCallMethodAction
+	{
+		private MethodBinderWithArgument _callbackMethod = new MethodBinderWithArgument();
+
+		protected override void Invoke(object parameter)
+		{
+			_callbackMethod.Invoke(MethodTarget, MethodName, parameter);
+		}
+	}
+}
